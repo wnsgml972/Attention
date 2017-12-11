4b7eb034823c3a883cfacda7a172bd2002a67aba
hscom007 <dbstjd1112@gmail.com>
2017-12-12 04:01:16 +0900

    synchronize user_profile_image and upload to firebase

--- app/src/main/java/kr/ac/hifly/attention/main/Main_Friend_Info_Activity.java
+++ app/src/main/java/kr/ac/hifly/attention/main/Main_Friend_Info_Activity.java
@@ -3,7 +3,9 @@ package kr.ac.hifly.attention.main;
 
 import android.content.Intent;
 import android.media.Image;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -13,12 +15,16 @@ import android.widget.TextView;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.resource.bitmap.CircleCrop;
 import com.bumptech.glide.request.RequestOptions;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
 import hifly.ac.kr.attention.R;
 import jp.wasabeef.glide.transformations.CropCircleTransformation;
 import kr.ac.hifly.attention.data.User;
+import kr.ac.hifly.attention.value.Values;
 
 public class Main_Friend_Info_Activity extends AppCompatActivity {
     private ImageView profileView;
@@ -26,6 +32,17 @@ public class Main_Friend_Info_Activity extends AppCompatActivity {
     private TextView stateTextView;
     private FloatingActionButton callFab;
     private FloatingActionButton messageFab;
+
+    ////////////성원 메인 프사 리스트뷰 눌렀을때 내부 프사 동기화 시키기
+    private FirebaseAuth mAuth;
+    public void setmAuth(FirebaseAuth mAuth) {
+        this.mAuth = FirebaseAuth.getInstance();
+    }
+    //private FirebaseUser user = mAuth.getCurrentUser();
+    private FirebaseStorage storage = FirebaseStorage.getInstance();
+
+    private StorageReference storageRef;
+    //////////////////////////////////
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,17 +52,37 @@ public class Main_Friend_Info_Activity extends AppCompatActivity {
         stateTextView = (TextView) findViewById(R.id.main_friend_info_state_textView);
         profileView = (ImageView) findViewById(R.id.activity_main_friend_profile);
 
-        Glide.with(this).load(R.drawable.main_friend_basic_icon)
-                .apply(RequestOptions.bitmapTransform(new CircleCrop()))
-                .into((profileView));
 
-        Glide.with(this).load("https://firebasestorage.googleapis.com/v0/b/attention-469ab.appspot.com/o/%EA%B7%B8%EB%A6%BC1.png?alt=media&token=b59f864d-b8c5-4907-9c99-dfac392d5ff1") .apply(RequestOptions.bitmapTransform(new CircleCrop())).thumbnail(0.1f).into(profileView);
+
+
+
         callFab = (FloatingActionButton)findViewById(R.id.main_friend_info_call_fab);
         messageFab = (FloatingActionButton) findViewById(R.id.main_friend_info_message_fab);
         User user = (User)getIntent().getSerializableExtra("object");
         if(user != null){
             nameTextView.setText(user.getName());
             stateTextView.setText(user.getStateMessage());
+
+            //////////////성원 이미지 내려받기 파이어베이스 레퍼런스 가져오기
+            mAuth = FirebaseAuth.getInstance();
+            storageRef = storage.getReferenceFromUrl("gs://attention-469ab.appspot.com/" + user.getUuid() + "/profile/profile.jpg");
+            storageRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                @Override
+                public void onSuccess(Uri uri) {
+                    Glide.with(getApplication()).load(uri).apply(RequestOptions.bitmapTransform(new CircleCrop())).thumbnail(0.1f).into(profileView);
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+
+                }
+            });
+            ////////////////////////////////////////////////////////////
+        }
+        else{
+            Glide.with(this).load(R.drawable.main_friend_basic_icon)
+                    .apply(RequestOptions.bitmapTransform(new CircleCrop()))
+                    .into((profileView));
         }
     }
 
@@ -60,4 +97,5 @@ public class Main_Friend_Info_Activity extends AppCompatActivity {
         intent.putExtra("object",(User)getIntent().getSerializableExtra("object"));
         startActivity(intent);
     }
+
 }
